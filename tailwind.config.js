7f32423e2ca1185a55560d04d300b7d1742b9ed2
Glauciene Luminato <seu-email@gmail.com>
2025-11-24 17:10:29 -0300

    Primeiro commit - projeto WhatsAppFood

---
+++ tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.html", "./scripts/**/*.js","./styles/*css"],
+  theme: {
+    fontFamily: {
+      sans: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        primary: {
+          light: "#ECB6B6",
+          DEFAULT: "#BD3C3C",
+          dark: "#A51010",
+          deep: "#BF1C1C",
+        },
+        offwhite: {
+          light: "#F7F7F7",
+          lighter: "#FCFCFC",
+        },
+      },
+      backgroundImage: {
+        "home": "url('/assets/bg.png')",
+      },
+    },
+  },
+  plugins: [],
+}
