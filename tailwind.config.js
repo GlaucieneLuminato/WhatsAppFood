5c243eb270192f6986a63e58c3b7f03640bbab86
Glauciene Luminato <seu-email@gmail.com>
2025-11-26 13:18:47 -0300

    ajuste no header

--- tailwind.config.js
+++ tailwind.config.js
@@ -19,7 +19,7 @@ module.exports = {
         },
       },
       backgroundImage: {
-        "home": "url('/whatsappfood/assets/bg.png')",
+        "home": "url('/assets/bg.png')",
       },
     },
   },
